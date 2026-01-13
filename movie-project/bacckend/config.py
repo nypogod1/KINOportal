a9baeb03dfb705d970afd4deef7492f9940f8855
DayTon <grig_vlasov18@mail.ru>
2026-01-13 13:51:20 +0300

    Update config.py

--- movie-project/bacckend/config.py
+++ movie-project/bacckend/config.py
@@ -1,1 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here-change-it'
+    
+    # База данных
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1612@localhost:5432/movies_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # CORS разрешённые домены
+    CORS_ORIGINS = [
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ]
 
